5611cc39bda8816baf39a0c92d160e73381a4499
Bruno-carneiro-da-silva <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-01-24 23:23:59 -0300

    :construction: creating the context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { FormEvent, useContext, useState } from 'react';
 import Modal from 'react-modal';
+import { TransactionsContext } from '../../TransactionsContext';
+import closeImg from '../../assets/close.svg';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import closeImg from '../../assets/close.svg';
-import { FormEvent, useState } from 'react';
 import { api } from '../../services/api';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,20 +13,25 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const {createTransaction} = useContext(TransactionsContext);
     const [type, setType] = useState('deposit');
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
 
     const handleCreateNewTransaction = (event: FormEvent) => {
         event.preventDefault();
-        const data = {
+
+        setType('');
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        createTransaction({
             title,
-            value,
+            amount,
             category,
             type
-        }
-        api.post('/transactions', data)
+        })
     }
     return (
         <Modal
@@ -51,8 +57,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input
                     type="number"
                     placeholder="Value"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
